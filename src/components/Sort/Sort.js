074fffa306ccfe0b261141092f10e5aedecb8c64
Milen961 <theheller178@gmail.com>
2023-08-07 14:58:52 +0300

    Add sort

---
+++ src/components/Sort/Sort.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Sort = ({ onSortOptionChange }) => {
+  const handleSortOptionChange = (option) => {
+    onSortOptionChange(option);
+  };
+
+  return (
+    <div className="sorting-dropdown">
+      <label htmlFor="sort">Sort by:</label>
+      <select
+        id="sort"
+        onChange={(e) => handleSortOptionChange(e.target.value)}
+      >
+        <option value="alphabetical-ascending">Alphabetical A-Z</option>
+        <option value="alphabetical-descending">Alphabetical Z-A</option>
+        <option value="price-ascending">Price (Low to High)</option>
+        <option value="price-descending">Price (High to Low)</option>
+      </select>
+    </div>
+  );
+};
+
+export default Sort;
