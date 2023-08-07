72001ac4e5d59d4a41b3bb51855edf077301fc83
Milen961 <theheller178@gmail.com>
2023-08-07 14:59:48 +0300

    Update header and app.js to work with sort

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
-import { useState } from 'react';
-//import ProductCounter from './components/ProductCounter/ProductCounter';
-
 import ProductTile from './components/ProductTile/ProductTile';
 
 function App() {
- 
+
   const products = [
     {
       id: 1,
@@ -24,31 +20,85 @@ function App() {
       id: 2,
       name: 'Product 2',
       description: 'This is the description of Product 2.',
+      price: 140,
+      ratings: 5,
+      image: 'product2.jpg', 
+    },
+    {
+      id: 3,
+      name: 'b',
+      description: 'This is the description of Product 2.',
       price: 150,
       ratings: 5,
       image: 'product2.jpg', 
       category: 'Shoes',
     },
-    // Add more products here
+    {
+      id: 4,
+      name: 'a',
+      description: 'This is the description of Product 2.',
+      price: 160,
+      ratings: 5,
+      image: 'product2.jpg', 
+      category: 'Shoes',
+    },
+    {
+      id: 4,
+      name: 'a',
+      description: 'This is the description of Product 2.',
+      price: 160,
+      ratings: 5,
+      image: 'product2.jpg', 
+      category: 'Bags',
+    },
+    
   ];
 
+ 
   const [selectedCategory, setSelectedCategory] = useState('Bags'); // Default category is "Bags"
+  const [sortOption, setSortOption] = useState('alphabetical-ascending'); // Default sorting option
 
- 
-  const filteredProducts = products.filter(
-    (product) => product.category === selectedCategory
-  );
-
- 
+  // Function to handle category change when clicking on the header
   const handleCategoryChange = (category) => {
     setSelectedCategory(category);
   };
 
+  // Function to handle sorting option change
+  const handleSortOptionChange = (option) => {
+    setSortOption(option);
+  };
+
+  // Function to get the filtered and sorted products based on category and sort option
+  const getFilteredAndSortedProducts = () => {
+    const filteredProducts = products.filter(
+      (product) => product.category === selectedCategory
+    );
+
+    switch (sortOption) {
+      case 'alphabetical-ascending':
+        return filteredProducts.slice().sort((a, b) => a.name.localeCompare(b.name));
+      case 'alphabetical-descending':
+        return filteredProducts.slice().sort((a, b) => b.name.localeCompare(a.name));
+      case 'price-ascending':
+        return filteredProducts.slice().sort((a, b) => a.price - b.price);
+      case 'price-descending':
+        return filteredProducts.slice().sort((a, b) => b.price - a.price);
+      default:
+        return filteredProducts;
+    }
+  };
+
+  const sortedProducts = getFilteredAndSortedProducts();
+
   return (
     <div className="App">
-      <Header onCategoryChange={handleCategoryChange} />
+      <Header
+        onCategoryChange={handleCategoryChange}
+        onSortOptionChange={handleSortOptionChange}
+      />
+      {/* Product Grid */}
       <div className="product-grid">
-        {filteredProducts.map((product) => (
+        {sortedProducts.map((product) => (
           <ProductTile key={product.id} product={product} />
         ))}
       </div>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,28 +1,29 @@
 import React from 'react';
+import Sort from '../Sort/Sort';
 
-const Header = ({ onCategoryChange }) => {
+const Header = ({ onCategoryChange, onSortOptionChange }) => {
   return (
     <header className="header">
     
       <div className="logo">
-       
+      
         <img src="logo.svg" alt="Logo" />
       </div>
 
-     
+   
       <nav className="nav-menu">
         <ul className="menu-list">
-          <li onClick={() => onCategoryChange('Bags')}>
-            Bags
-          </li>
-          <li onClick={() => onCategoryChange('Shoes')}>
-            Shoes
-          </li>
-         
+          <li onClick={() => onCategoryChange('Bags')}>Bags</li>
+          <li onClick={() => onCategoryChange('Shoes')}>Shoes</li>
+        
         </ul>
       </nav>
+
+    
+      <Sort onSortOptionChange={onSortOptionChange} />
     </header>
   );
 };
 
 export default Header;
+
