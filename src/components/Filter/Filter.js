672e53a319b54067f8b62d0654d4a1ae80e0f84c
Milen961 <theheller178@gmail.com>
2023-08-09 12:53:32 +0300

    Add filter component

---
+++ src/components/Filter/Filter.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+
+const Filter = ({ products, onFilterChange }) => {
+  const [minPrice, setMinPrice] = useState('');
+  const [maxPrice, setMaxPrice] = useState('');
+  const [minRating, setMinRating] = useState('');
+
+  const handleApplyFilter = () => {
+    const filteredProducts = products.filter((product) => {
+      const meetsPriceCriteria =
+        (minPrice === '' || product.price >= parseFloat(minPrice)) &&
+        (maxPrice === '' || product.price <= parseFloat(maxPrice));
+      
+      const meetsRatingCriteria =
+        minRating === '' || product.ratings >= parseInt(minRating);
+
+      return meetsPriceCriteria && meetsRatingCriteria;
+    });
+
+    onFilterChange(filteredProducts);
+  };
+
+  return (
+    <div className="filter">
+      <h3>Filter</h3>
+      <label htmlFor="minPrice">Min Price:</label>
+      <input
+        type="number"
+        id="minPrice"
+        value={minPrice}
+        onChange={(e) => setMinPrice(e.target.value)}
+      />
+      <label htmlFor="maxPrice">Max Price:</label>
+      <input
+        type="number"
+        id="maxPrice"
+        value={maxPrice}
+        onChange={(e) => setMaxPrice(e.target.value)}
+      />
+      <label htmlFor="minRating">Min Rating:</label>
+      <input
+        type="number"
+        id="minRating"
+        value={minRating}
+        onChange={(e) => setMinRating(e.target.value)}
+      />
+      <button onClick={handleApplyFilter}>Apply Filter</button>
+    </div>
+  );
+};
+
+export default Filter;
