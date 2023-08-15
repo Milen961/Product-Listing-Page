c1bfd80c18b595ebe2ca9c0443730b5ec030f951
Milen961 <theheller178@gmail.com>
2023-08-15 12:07:43 +0300

    Add div prodcut details and css to fix the alignment of the elements

--- src/components/ProductTile/ProductTile.js
+++ src/components/ProductTile/ProductTile.js
@@ -1,4 +1,3 @@
-
 const ProductTile = ({ product }) => {
   const { image, name, description, price, discountedPrice, ratings } = product;
 
@@ -8,28 +7,28 @@ const ProductTile = ({ product }) => {
 
   return (
     <div className="product-tile">
-  
       <img className="product-img" src={image} alt={name} />
-      <h3 className="product-name">{name}</h3>
-      <p className="product-description">{description}</p>
-      {discountedPrice ? (
-        <div className="price-container">
-          <span className="discounted-price">${discountedPrice}</span>{" "}
-          <span className="original-price">${price}</span>
+      <div className="product-details">
+        <h3 className="product-name">{name}</h3>
+        <p className="product-description">{description}</p>
+        {discountedPrice ? (
+          <div className="price-container">
+            <span className="discounted-price">${discountedPrice}</span>{" "}
+            <span className="original-price">${price}</span>
+          </div>
+        ) : (
+          <span className="price">${price}</span>
+        )}
+        <div className="ratings">
+          {Array.from({ length: ratings }).map((_, index) => (
+            <span key={index} className="star">&#9733;</span>
+          ))}
         </div>
-      ) : (
-        <span className="price">${price}</span>
-      )}
-      <div className="ratings">
-        {Array.from({ length: ratings }).map((_, index) => (
-          <span key={index} className="star">&#9733;</span>
-        ))}
+        <button onClick={handleAddToCart} className="add-to-cart-button">
+          Add to Cart
+        </button>
       </div>
-      <button onClick={handleAddToCart} className="add-to-cart-button">
-        Add to Cart
-      </button>
-      </div>
-    
+    </div>
   );
 };
 
